7517397ff940d06be0fa912917bc035bec214d60
LordPachimari <opachimari@gmail.com>
2024-12-09 03:06:50 +0300

    fix layout, remove image

--- apps/web/src/pages/_app/PageConfig/UnauthorizedLayout/index.tsx
+++ apps/web/src/pages/_app/PageConfig/UnauthorizedLayout/index.tsx
@@ -1,18 +1,16 @@
 import React, { FC, ReactElement } from 'react';
-import { Box, Image, SimpleGrid } from '@mantine/core';
+import { Box, Flex } from '@mantine/core';
 
 interface UnauthorizedLayoutProps {
   children: ReactElement;
 }
 
 const UnauthorizedLayout: FC<UnauthorizedLayoutProps> = ({ children }) => (
-  <SimpleGrid cols={{ base: 1, lg: 2 }} spacing="sm" w="100vw">
-    <Image h="100vh" src="/images/ship.svg" alt="App Info" visibleFrom="xl" />
-
-    <Box h="100vh" w="100%" pt={100} px={32}>
+  <Flex w="100vw">
+    <Box h="100vh" w="100%" miw={1000} pt={100} px={32}>
       {children}
     </Box>
-  </SimpleGrid>
+  </Flex>
 );
 
 export default UnauthorizedLayout;
